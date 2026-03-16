22d2bbc2c91d184d6489a384a9510747db3ffb3c
Lex Le <sonle@Sons-MacBook-Pro.local>
2026-03-16 23:05:55 +1100

    fix some bugs

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,16 @@
+'use client'
+
 import siteMetadata from '@/data/siteMetadata'
 import headerNavLinks from '@/data/headerNavLinks'
 import Logo from '@/data/logo.svg'
 import Link from './Link'
-import MobileNav from './MobileNav'
+import dynamic from 'next/dynamic'
 import ThemeSwitch from './ThemeSwitch'
 import SearchButton from './SearchButton'
+
+const MobileNav = dynamic(() => import('./MobileNav'), {
+  ssr: false,
+})
 
 const Header = () => {
   let headerClass = 'flex items-center w-full bg-white dark:bg-gray-950 justify-between py-10'
